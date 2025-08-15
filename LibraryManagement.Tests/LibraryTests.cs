2309e921be3190f336889ebc5d5a1c3e050f3d1c
Katelyn <katelyn.lai@outlook.com>
2025-08-15 10:23:58 +0100

    basic starting test

--- LibraryManagement.Tests/LibraryTests.cs
+++ LibraryManagement.Tests/LibraryTests.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using NUnit.Framework;
+using LibraryManagement;
+
+namespace LibraryManagement.Tests
+{
+    public class LibraryTests
+    {
+        [Test]
+        public void BorrowBook_WhenAvailable_ReturnsTrue()
+        {
+            // Arrange
+            var library = new Library();
+            var patron = new Patron("Katelyn");
+            var book = new Book("Pride and Prejudice", "Jane Austen");
+            library.AddBook(book);
+            library.AddPatron(patron);
+
+            // Act
+            var result = library.BorrowBook(patron, book);
+
+            // Assert
+            Assert.That(result, Is.True);
+            Assert.That(book.IsAvailable, Is.False);
+            Assert.That(patron.BorrowedBooks, Has.Member(book));
+
+        }
+    }
+}
